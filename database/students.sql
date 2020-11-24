1c179b8c416416501ed76fb37b891e4b92bc6d42
Isaack Motanya <isaackmotanya6@gmail.com>
2020-11-24 15:13:48 +0300

    Update: added database that will hold student details

--- database/students.sql
+++ database/students.sql
@@ -1,1 +1,5 @@
-CREATE TABLE `students`.`students` ( `id` INT NOT NULL AUTO_INCREMENT ,  `student_id` INT NOT NULL ,  `full_name` INT NOT NULL ,  `email` VARCHAR(255) NOT NULL ,  `phone` VARCHAR(25) NOT NULL ,  `address` VARCHAR(100) NOT NULL ,  `entry_points` INT(100) NOT NULL ,    PRIMARY KEY  (`id`),    UNIQUE  (`student_id`),    UNIQUE  (`email`)) ENGINE = InnoDB;
+CREATE DATABASE students;
+
+use students;
+
+CREATE TABLE `students`.`students` ( `id` INT NOT NULL AUTO_INCREMENT ,  `student_id` INT NOT NULL ,  `full_name` VARCHAR(10) NOT NULL ,  `email` VARCHAR(255) NOT NULL ,  `phone` VARCHAR(25) NOT NULL ,  `address` VARCHAR(100) NOT NULL ,  `entry_points` INT(100) NOT NULL ,    PRIMARY KEY  (`id`),    UNIQUE  (`student_id`),    UNIQUE  (`email`)) ENGINE = InnoDB;
